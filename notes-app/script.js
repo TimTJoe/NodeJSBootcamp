7852baeb45fc30559c72a7f08adf14bbbfa44d74
Timothy T. Joe <106974662+TimTJoe@users.noreply.github.com>
2024-06-07 12:20:24 +0000

    Added styling to notes-app

--- notes-app/script.js
+++ notes-app/script.js
@@ -1,34 +1,40 @@
 const db = window.localStorage;
 const form = document.getElementById("form");
 const content = document.getElementById("content");
-const time = document.getElementById("time");
-const body = document.getElementById("body");
+const submit = document.getElementById("button");
 
-window.addEventListener("DOMContentLoaded", () => {
+window.onload = () => {
   displayNotes(JSON.parse(db.getItem("notes")));
-});
+  window.history.pushState({ id: null }, { name: "init" });
+  console.log(window.history.state.id)
+};
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  let text = document.getElementById("textarea").value;
+  // let updateID = window.history.state.id || null;
+  const text = document.getElementById("textarea").value;
 
-  let date = new Date();
-  date = date.toISOString().substring(0, 10);
-
+  let date = getDate();
   let note = { id: Date.now(), text, date };
-  let response = createNote(note);
-  displayNotes(response);
+
+  if (window.history.state.id !== null) {
+    updatedNotes(window.history.state.id, note);
+  } else {
+    let response = createNote(note);
+    displayNotes(response);
+  }
   window.location.reload();
 });
 
 function createNote(note) {
-  let allNotes = JSON.parse(db.getItem("notes"));
-  if (allNotes == null) {
+  let notes = JSON.parse(db.getItem("notes"));
+
+  if (notes == null) {
     db.setItem("notes", JSON.stringify([note]));
     return JSON.parse(db.getItem("notes"));
   } else {
-    allNotes.push(note);
-    db.setItem("notes", JSON.stringify(allNotes));
+    notes.push(note);
+    db.setItem("notes", JSON.stringify(notes));
     return JSON.parse(db.getItem("notes"));
   }
 }
@@ -46,17 +52,25 @@ function deleteNotes(id) {
 
 function displayNotes(notes) {
   notes
-    ?.sort()
+    ?.sort((a, b) => new Date(a.date) - new Date(b.date))
     ?.reverse()
     .map((note) => {
       let article = `
-      <article class="note note-section" id="note">
-        <div class="note-content">
-            <time class="note-time" id="time">${note.date}</time>
-            <p class="note-body" id="body">${note.text}</p>
-        </div>
-        <footer class="note-footer" >
-          <button class="deleteButton" id=${note.id}>Delete</button>
+      <article class="note" id="note">
+        <header>
+            <time class="time" id="time">${note.date}</time>
+        </header>
+        <p class="body" id="body">
+            ${note.text}
+        </p>
+        <footer class="actions" >
+          <button class="edit" id="e${note.id}">
+            Edit
+          </button>
+          <button class="delete" id=${note.id}>
+            Delete
+          </button>
+        
         </footer>
       </article>
     `;
@@ -67,5 +81,38 @@ function displayNotes(notes) {
         deleteNotes(note.id);
         window.location.reload();
       });
+      document.getElementById(`e${note.id}`).addEventListener("click", (e) => {
+        let id = e.target.id.substring(1);
+        textarea.value = note.text;
+        submit.style.backgroundColor = "var(--orange)";
+        textarea.style.outline = "thin solid var(--xlite-gray)";
+
+        window.history.pushState({ id }, { name: "edit note" });
+      });
     });
+}
+
+function getDate() {
+  let _date = new Date();
+  let date = new Intl.DateTimeFormat("us-EN", {
+    year: "numeric",
+    month: "short",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+  }).format(_date);
+
+  return date;
+}
+
+function updatedNotes(id, note) {
+  let notes = JSON.parse(db.getItem("notes"));
+  let index = notes?.findIndex((item) => item.id == id);
+
+  notes[index].text = note.text;
+  notes[index].date = getDate();
+
+  db.setItem("notes", JSON.stringify(notes));
+  window.history.pushState({ id: null }, { name: "no note" });
+  console.log(JSON.parse(db.getItem("notes")));
 }
