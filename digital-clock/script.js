5e1196894dc1b28220ba6619c54b71e50a642527
Timothy T. Joe <106974662+TimTJoe@users.noreply.github.com>
2024-06-08 13:32:55 +0000

    5. Design your clock 6. Test and make sure it’s working 7. Push the project to GitHub, create a GitHub page, and share the link in the group

--- digital-clock/script.js
+++ digital-clock/script.js
@@ -1,10 +1,22 @@
 function displayTime() {
-    let date = new Date()
-    let content = document.getElementById("content")
-    content.innerHTML = `
-    <span>${date.toLocaleTimeString()}
-    `;
-    console.log("interval")
+  let date = new Date();
+  let _date = new Date().toLocaleDateString("en-US", {
+    weekday: "long",
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  });
+
+  let hour = date.getHours();
+  let min = date.getMinutes();
+  let sec = date.getSeconds();
+  var meridiem = hour >= 12 ? "PM" : "AM";
+
+  document.getElementById("hr").innerHTML = hour;
+  document.getElementById("min").innerHTML = min;
+  document.getElementById("sec").innerHTML = sec;
+  document.getElementById("meridiem").innerHTML = meridiem;
+  document.getElementById("date").innerHTML = _date;
 }
 
 setInterval(displayTime, 100);
