c8e72eec91ccdc5a54b0917276e5f89562baea66
Timothy T. Joe <106974662+TimTJoe@users.noreply.github.com>
2024-06-14 20:27:17 +0000

    implemented box selection for tic tac toe game

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -1,9 +1,21 @@
 let mark = "x";
 let boxes = document.querySelectorAll(".box");
+let winningOptions = ["123", "147", "159", "258", "369", "357", "456", "789"];
+let computerMoves = 0;
+let HumanMoves = 0;
 
 boxes.forEach((box) => {
   box.addEventListener("click", (event) => {
-    switchTurn();
+    if (isHuman()) {
+      playHuman(event);
+      endGame();
+    }
+    if (isComputer()) {
+      setTimeout(() => {
+        playComputer();
+        endGame();
+      }, 2000);
+    }
   });
 });
 
@@ -17,4 +29,48 @@ function isEmpty(nodeId) {
 function switchTurn() {
   let turnText = document.getElementById("turn-text");
   turnText.innerText = turnText.innerText == "Computer" ? "Human" : "Computer";
+}
+
+function isHuman() {
+  if (document.getElementById("turn-text").innerText == "Human") {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isComputer() {
+  if (document.getElementById("turn-text").innerText == "Computer") {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function playHuman(node) {
+  node.target.innerText = "X";
+  ++HumanMoves;
+  switchTurn();
+}
+
+function playComputer() {
+  for (const box in boxes) {
+    let randomNum = Math.floor(Math.random() * 9);
+    if (boxes[randomNum].innerText === "") {
+      boxes[randomNum].innerText = "O";
+      break;
+    } else if (computerMoves == 4) {
+      break;
+    }
+  }
+  ++computerMoves;
+  switchTurn();
+}
+
+function endGame() {
+  if (HumanMoves === 5) {
+    alert("human moves ends...");
+  } else if (computerMoves == 4) {
+    alert("computer move ends...");
+  }
 }
