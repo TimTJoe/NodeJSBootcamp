9c4d8ef155963dec75fcf1bca53e8d1b5902101a
Timothy T. Joe <106974662+TimTJoe@users.noreply.github.com>
2024-06-24 15:42:46 +0000

    more works

--- quiz-game/script.js
+++ quiz-game/script.js
@@ -2,11 +2,36 @@ const questionsURL = "./questions.json";
 const htmlBtn = document.getElementById("htmlBtn");
 const jsBtn = document.getElementById("jsBtn");
 const cssBtn = document.getElementById("cssBtn");
-
-window.addEventListener("DOMContentLoaded", (e) => {});
+const questionIndex = [
+  "q1",
+  "q2",
+  "q3",
+  "q4",
+  "q5",
+  "q6",
+  "q7",
+  "q8",
+  "q8",
+  "q9",
+  "q10",
+];
 
 htmlBtn.addEventListener("click", async (e) => {
   let data = await readJSONFile(questionsURL);
+  data = data["html"][questionIndex[1]];
+  console.log(data);
+});
+
+cssBtn.addEventListener("click", async (e) => {
+  let data = await readJSONFile(questionsURL);
+  data = data["css"];
+  console.log(data);
+});
+
+jsBtn.addEventListener("click", async (e) => {
+  let data = await readJSONFile(questionsURL);
+  data = data["js"];
+
   console.log(data);
 });
 
@@ -14,4 +39,28 @@ async function readJSONFile(url) {
   const response = await fetch(url);
   const data = await response.json();
   return data;
+}
+
+function displayQuestion(question) {
+  console.log(question);
+}
+
+function getData(data, type, index) {
+  return data[type][index];
+}
+
+function getRandomQuestion(data) {
+  const questionIndex = [
+    "q1",
+    "q2",
+    "q3",
+    "q4",
+    "q5",
+    "q6",
+    "q7",
+    "q8",
+    "q8",
+    "q9",
+    "q10",
+  ];
 }
