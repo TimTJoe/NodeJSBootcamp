f5ff8b499312809d5712f82f0d813a559d55b63b
Princess B.  Washington <106974662+TimTJoe@users.noreply.github.com>
2024-07-29 14:59:28 +0000

    election app: updated voter registration

--- election-app/server.js
+++ election-app/server.js
@@ -15,11 +15,17 @@ const db = new sqlite3.Database('./elections.db')
 db.serialize(() => {
 
     db.run('CREATE TABLE IF NOT EXISTS auth (id INTEGER  PRIMARY KEY AUTOINCREMENT, username VARCHAR(50) NOT NUll UNIQUE, password VARCHAR(50) NOT NULL, user_id INTEGER)')
+
     db.run('CREATE TABLE IF NOT EXISTS roles (id INTEGER  PRIMARY KEY AUTOINCREMENT, role VARCHAR(50) NOT NUll)')
-    db.run('CREATE TABLE IF NOT EXISTS users (id INTEGER  PRIMARY KEY AUTOINCREMENT, first_name VARCHAR(50) NOT NUll, middle_name VARCHAR(50) NULL, last_name VARCHAR(50) NOT NULL, DOB DATE NOT NULL)')
+
+    db.run('CREATE TABLE IF NOT EXISTS users (id INTEGER  PRIMARY KEY AUTOINCREMENT, first_name VARCHAR(50) NOT NUll, middle_name VARCHAR(50) NULL, last_name VARCHAR(50) NOT NULL, DOB DATE NOT NULL, photo BLOB NULL, role_id INT NOT NULL)')
+
     db.run('CREATE TABLE IF NOT EXISTS parties (id INTEGER  PRIMARY KEY AUTOINCREMENT, party VARCHAR(50) NOT NUll, logo BLOB NULL)')
+
     db.run('CREATE TABLE IF NOT EXISTS positions (id INTEGER  PRIMARY KEY AUTOINCREMENT, position VARCHAR(50) NOT NUll)')
+
     db.run('CREATE TABLE IF NOT EXISTS candidates (id INTEGER  PRIMARY KEY AUTOINCREMENT, first_name VARCHAR(50) NOT NUll, middle_name VARCHAR(50) NULL, last_name VARCHAR(50), position_id INTEGER NOT NULL, party_id INTEGER NOT NULL, photo BLOB NOT NULL)')
+
     db.run('CREATE TABLE IF NOT EXISTS votes (id INTEGER  PRIMARY KEY AUTOINCREMENT, candidate_id INTEGER NOT NULL, vote INT NOT NULL)')
 
 })
@@ -57,15 +63,23 @@ app.post("/login", (req, res) => {
 
 
 app.get("/voter-registration", (req, res) => {
-    res.render("voter-registration.ejs")
+     // Query the database to find a matching username and password
+     db.all('SELECT * FROM roles', function(err, row) {
+        if (row) {
+            console.log(row)
+            res.render("voter-registration.ejs", {row})
+        } else {
+            console.error(err);
+        }
+    });
 })
  
 // Handle POST request to /voter-registration endpoint
 app.post("/voter-registration", (req, res) => {
         // Destructure required fields from request body
-        const {first_name, middle_name, last_name, DOB, username, password} = req.body;
+        const {first_name, middle_name, last_name, DOB, username, password, photo, role} = req.body;
         // Insert user information into 'users' table in the database
-        db.run("INSERT INTO users VALUES(?,?,?,?,?)", [null, first_name, middle_name, last_name, DOB], function(err) { 
+        db.run("INSERT INTO users VALUES(?,?,?,?,?,?,?)", [null, first_name, middle_name, last_name, DOB,photo, role], function(err) { 
             if(err) { 
                 // If there's an error, throw it
                 console.error(err)
